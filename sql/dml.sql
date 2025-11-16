2caac0eef35bbb48188e0079c3b2b7961c7d089d
doyeon <doyun20445@gmail.com>
2025-11-16 22:06:17 +0900

    feat: 예시 sql 생성

---
+++ sql/dml.sql
@@ -0,0 +1,48 @@
+INSERT INTO restaurant (restaurant_name, location) VALUES ('백소정 인하대후문점', '인천광역시 미추홀구 인하로 73-1 1층');
+INSERT INTO restaurant (restaurant_name, location) VALUES ('인하칼국수 인하대후문점', '인천 미추홀구 경인남길30번길 35-1 1층');
+INSERT INTO restaurant (restaurant_name, location) VALUES ('궁중보쌈 인하대후문점', '인천 미추홀구 경인남길30번길 36 2층');
+INSERT INTO restaurant (restaurant_name, location) VALUES ('미식당', '인천 미추홀구 인하로67번길 6 1층 미식당');
+
+# 백소정
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('1', '오꼬노미카츠', '일식', 12900);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('1', '체다치즈카츠', '일식', 12900);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('1', '지루소바 돈카츠', '일식', 14500);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('1', '머쉬룸 포크카레', '일식', 9500);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('1', '에비텐어묵우동', '일식', 9900);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('1', '마제소바 에비텐', '일식', 13500);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('1', '모짜렐라 치즈카츠', '일식', 12900);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('1', '냉소바', '일식', 9500);
+
+# 인하칼국수
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('2', '들깨칼국수', '한식', 9500);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('2', '해물칼국수', '한식', 10000);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('2', '바지락칼국수', '한식', 8500);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('2', '매운해물칼국수', '한식', 11000);
+
+# 궁중보쌈
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('3', '지짐보쌈정식', '한식', 9500);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('3', '매운지짐보쌈정식', '한식', 10000);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('3', '마늘지짐보쌈정식', '한식', 10000);
+
+# 미식당
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('4', '마라크림파스타', '양식', 9500);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('4', '스파이시까르보나라', '양식', 8900);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('4', '쉬림프바질크림파스타', '양식', 8900);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('4', '쉬림프베이컨로제파스타', '양식', 9500);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('4', '알리오올리오', '양식', 6900);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('4', '봉골레파스타', '양식', 8900);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('4', '해산물스파이시올리오', '양식', 9500);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('4', '아라비아타파스타', '양식', 8900);
+INSERT INTO menu (restaurant_id, menu_name, category, price) VALUES ('4', '뽀모도로파스타', '양식', 8900);
+
+
+
+
+
+
+
+
+
+
+
+
