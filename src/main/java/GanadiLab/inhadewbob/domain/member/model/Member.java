f6a06910237c09b3ef25fc5705ec72ca2fe9bacc
doyeon <doyun20445@gmail.com>
2025-11-16 22:06:36 +0900

    feat: Member 엔터티 생성

---
+++ src/main/java/GanadiLab/inhadewbob/domain/member/model/Member.java
@@ -0,0 +1,40 @@
+package GanadiLab.inhadewbob.domain.member.model;
+
+import GanadiLab.inhadewbob.global.base.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "member")
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false)
+    String username;
+
+    @Column(nullable = false)
+    String password;
+
+    @Column(nullable = false)
+    String nickname;
+
+    @Column(name = "weakly_budget", nullable = true)
+    Integer weeklyBudget;
+
+    @Column(name = "eatout_count", nullable = false)
+    Integer eatoutCount;
+
+    @Column(nullable = false)
+    Mode mode;
+}
