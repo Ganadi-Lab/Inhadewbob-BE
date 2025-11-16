3e201a0063119ed766e16540abf18c23442f7afb
doyeon <doyun20445@gmail.com>
2025-11-16 12:28:07 +0900

    initial commit

---
+++ src/main/java/GanadiLab/inhadewbob/InhadewbobApplication.java
@@ -0,0 +1,13 @@
+package GanadiLab.inhadewbob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InhadewbobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InhadewbobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GanadiLab/inhadewbob/InhadewbobApplicationTests.java
@@ -0,0 +1,13 @@
+package GanadiLab.inhadewbob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InhadewbobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
