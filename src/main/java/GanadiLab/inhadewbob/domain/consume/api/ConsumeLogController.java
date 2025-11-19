08a852dbc11afcd22e27f78975e704164e87b1d2
jiyun421 <ju4480@naver.com>
2025-11-19 22:56:04 +0900

    chore: /consumes 경 경로 수정

--- src/main/java/GanadiLab/inhadewbob/domain/consume/api/ConsumeLogController.java
+++ src/main/java/GanadiLab/inhadewbob/domain/consume/api/ConsumeLogController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/consume")
+@RequestMapping("/consumes")
 public class ConsumeLogController {
 
     private final ConsumeLogService consumeLogService;
