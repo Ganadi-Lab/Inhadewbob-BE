9414c9855d90e21e0ae032d92deb0a4f3e30cc30
doyeon <doyun20445@gmail.com>
2025-11-23 23:47:10 +0900

    fix: 룰렛 돌릴 때 카테고리 여러개 선택 가능하도록 수정

--- src/main/java/GanadiLab/inhadewbob/domain/menu/service/MenuService.java
+++ src/main/java/GanadiLab/inhadewbob/domain/menu/service/MenuService.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface MenuService {
 
     /* 식당 메뉴 랜덤 조회 */
-    MenuDTO.Response getMenusByRoulette(LocalDate date, String category, Integer price, Long memberId);
+    MenuDTO.Response getMenusByRoulette(LocalDate date, List<String> categories, Integer price, Long memberId);
 
     /* 추천 예산 조회 */
     MenuDTO.RecommendResponse getRecommendation(LocalDate date, Long memberId);
