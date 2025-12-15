4943227ea4e85b512dbb7da705429725931a7098
jiyun421 <ju4480@naver.com>
2025-12-15 12:18:50 +0900

    feat: 식단 기록 등록 시 수기 입력한 내용 저장하는 엔티티 추가

--- src/main/java/GanadiLab/inhadewbob/domain/manualDietDetail/repository/ManualDietDetailRepository.java
+++ src/main/java/GanadiLab/inhadewbob/domain/manualDietDetail/repository/ManualDietDetailRepository.java
@@ -1,4 +1,14 @@
 package GanadiLab.inhadewbob.domain.manualDietDetail.repository;
 
-public interface ManualDietDetailRepository {
+import GanadiLab.inhadewbob.domain.manualDietDetail.model.ManualDietDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import GanadiLab.inhadewbob.domain.diet.model.DietLog;
+
+public interface ManualDietDetailRepository
+        extends JpaRepository<ManualDietDetail, Long> {
+
+    Optional<ManualDietDetail> findByDietLog(DietLog dietLog);
 }
+
